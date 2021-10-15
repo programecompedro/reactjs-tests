7f7476d3310972e50c03d27e4b2a52622e5c9ae3
Pedro <pdemeu@outlook.com>
2021-10-15 15:16:16 -0300

    finally

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import Conta from './Conta';
 
 describe ("Componente da Conta", () => {
@@ -9,5 +9,15 @@ describe ("Componente da Conta", () => {
         const saldo = screen.getByTestId('saldo-conta');
 
         expect(saldo.textContent).toBe("R$ 1000");
+    })
+    it("Chama a função de realizar transação, quando o botão é clicado", () => {
+
+        const funcaoRealizarTransacao = jest.fn();
+
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao}/>)
+
+        fireEvent.click(screen.getByText('Realizar operação'));
+
+        expect(funcaoRealizarTransacao).toHaveBeenCalled()
     })
 })
